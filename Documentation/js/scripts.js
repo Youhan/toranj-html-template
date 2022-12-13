3a6ce3b4bc31381d47184ff2e6ba7f07c6ad289e
Alireza <ar.jahandideh@gmail.com>
2022-12-13 02:54:10 -0600

    Init

---
+++ Documentation/js/scripts.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash,
+	    $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+
+
+
+	// hide #back-top first
+	$("#back-top").hide();
+	
+	// fade in #back-top
+	$(function () {
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 300) {
+				$('#back-top').fadeIn();
+			} else {
+				$('#back-top').fadeOut();
+			}
+		});
+
+		// scroll body to 0px on click
+		$('#back-top a').click(function () {
+			$('body,html').animate({
+				scrollTop: 0
+			}, 800);
+			return false;
+		});
+	});
+
+
+	$('pre').each(function(){
+       var $pre = $(this);
+       $pre.addClass('prettyprint linenums');
+       $pre.html($pre.html().replace(/</g, '&lt;').replace(/>/g, '&gt;'));
+    });
+});
